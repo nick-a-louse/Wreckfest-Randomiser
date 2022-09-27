09d725dc591173d4440d7c52bd0a59559e2d4c11
Nick <nick.jeffordhorn@gmail.com>
2022-09-27 22:51:18 +0100

    Adding favicon, deleting junk to retrieve json file

--- index.js
+++ index.js
@@ -1,26 +1,14 @@
-const getJSON(jsonUrl) = async url => {
-    const response = await fetch(jsonUrl);
-    if(!response.ok) // check if response worked (no 404 errors etc...)
-        throw new Error(response.statusText);
-    
-    const data = response.json(); // get JSON from the response
-    return data; // returns a promise, which resolves to this data value
-    }
+<!--./index.js-->
 
-console.log("Fetching track data...");
-const trackData = getJSON('tracks.json').then(data => {
-//    console.log(data);
-}).catch(error => {
-    console.error(error);
-});
-console.log(trackData);
+const url = "./cars.json";
 
-console.log("Fetching car data...");
-const carData = getJSON('cars.json').then(data => {
-    carData = data
-//    console.log(data);
-}).catch(error => {
-    console.error(error);
-});
-console.log(carData);
+const fetchJson = async () => {
+  try {
+    const data = await fetch(url);
+    const response = await data.json();  
+  } catch (error) {
+    console.log(error);
+  }
+ };
 
+ fetchJson();
