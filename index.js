743f52c9fb59940a5e12e57916341b8a612e3c0d
Nick <nick.jeffordhorn@gmail.com>
2022-09-27 08:57:58 +0100

    Replaced the json fetch methods

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-// fetch('cars.json')
-//     .then((response) => response.json())
-//     .then((json) => console.log(json));
-
-
-// fetch('tracks.json')
-//     .then((response) => response.json())
-//     .then((json) => console.log(json));
-
-const getJSON = async url => {
-    const response = await fetch('cars.json');
+const getJSON(jsonUrl) = async url => {
+    const response = await fetch(jsonUrl);
     if(!response.ok) // check if response worked (no 404 errors etc...)
         throw new Error(response.statusText);
     
     const data = response.json(); // get JSON from the response
     return data; // returns a promise, which resolves to this data value
     }
-    
-console.log("Fetching data...");
+ 
+console.log("Fetching track data...");
+getJSON('tracks.json').then(data => {
+    console.log(data);
+}).catch(error => {
+    console.error(error);
+});
+
+console.log("Fetching car data...");
 getJSON('cars.json').then(data => {
     console.log(data);
 }).catch(error => {
     console.error(error);
 });
+
