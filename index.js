05e9bcfeac5e75738f104bb1cfd96cc15ddd6f12
Nick <nick.jeffordhorn@gmail.com>
2022-09-28 21:17:04 +0100

    Finally got javascript to read out of the json file and create an array

--- index.js
+++ index.js
@@ -1,14 +1,62 @@
 <!--./index.js-->
 
-const url = "./cars.json";
+// Function to retrieve the car json file
 
-const fetchJson = async () => {
-  try {
-    const data = await fetch(url);
-    const response = await data.json();  
-  } catch (error) {
-    console.log(error);
-  }
- };
+async function getCars() {
+    let url = './cars.json';
+    try {
+        let res = await fetch(url);
+        return await res.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
 
- fetchJson();
+// Funtion to create and array of cars. -> Still need to make dynamic and add extra filters
+
+var filteredCars = [];
+
+async function renderCars() {
+    let cars = await getCars();
+    let html = '';
+
+    cars.forEach(car => {
+        if (car.Class == "A") {
+            var tempCar = (car.Index);
+            filteredCars.push((tempCar));  
+        };
+    });
+
+console.log(filteredCars);
+
+}
+
+renderCars();
+
+// async function getCars() {
+//     let url = './cars.json';
+//     try {
+//         let res = await fetch(url);
+//         return await res.json();
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+// async function renderCars() {
+//     let cars = await getCars();
+//     let html = '';
+//     let filteredCars = '';
+//     cars.forEach(car => {
+//         let htmlSegment = `<div class="user">
+//                             <h2>${car.Index} ${car.Name}</h2>
+//                         </div>`;
+
+//         html += htmlSegment;
+//     });
+
+//     let container = document.querySelector('.container');
+//     container.innerHTML = html;
+// }
+
+// renderCars();
